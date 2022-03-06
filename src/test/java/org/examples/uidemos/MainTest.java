dc210abc6469df3ca8a1611a1b1a70ee41fc6cae
mourav2222 <mourav2222@gmail.com>
2022-03-06 16:37:21 +0100

    add safari support

--- src/test/java/org/examples/uidemos/MainTest.java
+++ src/test/java/org/examples/uidemos/MainTest.java
@@ -1,35 +1,52 @@
 package org.examples.uidemos;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import java.net.URL;
 import java.nio.channels.SelectableChannel;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 class MainTest {
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+//        Configuration.browser = "chrome";
 //        Configuration.browser = "firefox";
-        Configuration.remote = "http://192.168.0.158:4444/wd/hub";
+        Configuration.browser = "edge";
+//        Configuration.browser = "safari";
+
+        Configuration.remote = "http://localhost:4444/wd/hub";
+
+//        Configuration.remote = "http://192.168.0.158:4444/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
 //        capabilities.setCapability("enableVNC", true);
         Map<String, Object> map = new HashMap<>();
         map.put("enableVNC", true);
         map.put("enableVideo", true);
         capabilities.setCapability("selenoid:options", map);
+
+        if (Configuration.browser.equals("edge")) {
+            capabilities.setBrowserName("MicrosoftEdge");
+        }
+
         Configuration.browserCapabilities = capabilities;
+        if (Configuration.browser.equals("safari")) {
+            Configuration.browserCapabilities = getSafariCaps();
+        }
+
     }
 
     @Test
@@ -37,10 +54,28 @@ class MainTest {
 
         open("https://todomvc.com/examples/angularjs/");
 
+        RemoteWebDriver wd = (RemoteWebDriver) WebDriverRunner.getWebDriver();
+
+        Capabilities cap = wd.getCapabilities();
+        String browserName = cap.getBrowserName().toLowerCase();
+        String browserVersion = cap.getBrowserVersion();
+        System.out.println("browser = " + browserName);
+        System.out.println("browser version = " + browserVersion);
         $("input.new-todo").sendKeys("Show Selenide");
         $("input.new-todo").pressEnter();
 
         $(withText("Show Selenide"))
                 .shouldBe(visible);
+    }
+
+    // @Test
+    private SafariOptions getSafariCaps() throws Exception {
+        SafariOptions safariOptions = new SafariOptions();
+        safariOptions.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVNC", true);
+        }});
+        return safariOptions;
+        //RemoteWebDriver driver = new RemoteWebDriver(new URL("http://moon.aerokube.local:4444/wd/hub/"), safariOptions);
+        //driver.quit();
     }
 }
